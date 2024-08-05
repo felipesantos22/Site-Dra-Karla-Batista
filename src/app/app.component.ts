61c022731187e8af6a4424ea753d9a79bf31d000
felipesantos22 <felipevs2018@gmail.com>
2024-08-05 17:14:52 -0300

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./componets/header/header.component";
+import { Section1Component } from './componets/section1/section1.component';
+import { Section3Component } from './componets/section3/section3.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, Section1Component,Section3Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
