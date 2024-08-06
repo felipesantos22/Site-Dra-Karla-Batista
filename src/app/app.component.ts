0a96e0295e34a80d1c1b2f5214a19763659749a7
felipesantos22 <felipevs2018@gmail.com>
2024-08-06 15:22:45 -0300

    change home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./componets/header/header.component";
 import { Section1Component } from './componets/section1/section1.component';
 import { Section3Component } from './componets/section3/section3.component';
+import { Section2Component } from "./componets/section2/section2.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, Section1Component,Section3Component],
+  imports: [HeaderComponent, Section1Component, Section3Component, Section2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/componets/section2/section2.component.ts
+++ src/app/componets/section2/section2.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { UppercasePipe } from '../../pipes/uppercase.pipe';
 
 @Component({
   selector: 'app-section2',
   standalone: true,
-  imports: [],
+  imports: [UppercasePipe],
   templateUrl: './section2.component.html',
   styleUrl: './section2.component.css'
 })
 export class Section2Component {
+
+  name = "Dra. Karla Batista"
 
 }
