540fc3aacde97a10ebefb230084648d25c36a336
felipesantos22 <felipevs2018@gmail.com>
2024-08-09 18:47:51 -0300

    insert route notfounf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './componets/header/header.component';
-import { Section1Component } from './componets/section1/section1.component';
-import { Section3Component } from './componets/section3/section3.component';
-import { Section2Component } from './componets/section2/section2.component';
-import { Section4Component } from './componets/section4/section4.component';
 import { FooterComponent } from './componets/footer/footer.component';
 
 @Component({
@@ -12,10 +7,6 @@ import { FooterComponent } from './componets/footer/footer.component';
   standalone: true,
   imports: [
     HeaderComponent,
-    Section1Component,
-    Section2Component,
-    Section3Component,    
-    Section4Component,
     FooterComponent,
   ],
   providers: [],
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { Section1Component } from "../../componets/section1/section1.component";
+import { Section2Component } from "../../componets/section2/section2.component";
+import { Section3Component } from "../../componets/section3/section3.component";
+import { Section4Component } from "../../componets/section4/section4.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [Section1Component, Section2Component, Section3Component, Section4Component],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
